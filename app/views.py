27319942a1ec1607f3205953f5da9cb83804ee4d
jey <jey@jeys-MacBook-Pro.local>
2020-03-26 21:31:24 +0300

    Add initial files

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def index(request):
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # context = {'latest_question_list': latest_question_list}
+    # return render(request, 'polls/index.html', context)
+    return render(request, 'blog/index.html')
