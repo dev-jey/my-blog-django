8ad07a4f15d3514a8ece790007eaf7606f9513ea
jey <jey@jeys-MacBook-Pro.local>
2020-03-26 23:42:42 +0300

    Add models for articles, users, categories

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from app.models import Article, User, Category
+
+class ArticleAdmin(admin.ModelAdmin):
+    # exclude = ('minute_read',)
+    pass
+admin.site.register(Article, ArticleAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # exclude = ('minute_read',)
+    pass
+admin.site.register(Category, CategoryAdmin)
+
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(User, UserAdmin)
+
+admin.site.site_header = "Dev-Jey"
+admin.site.site_title = "Dev-Jey"
