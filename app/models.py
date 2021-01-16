d11636e4da3778ab38dd9aa7103ab98f7cbc6693
dev-jey <savalijey@gmail.com>
2021-01-16 18:00:38 +0300

    Fix blog categories

--- app/models.py
+++ app/models.py
@@ -71,7 +71,7 @@ def photo_delete(sender, instance, **kwargs):
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
-    image = CloudinaryField('image',folder='avatars/categories')
+    image = CloudinaryField('image',folder='myblog/categories')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
